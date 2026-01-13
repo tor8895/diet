c1987d1891072203721e6d19258a9bb9dea6f529
tor8895 <tor8895@gmail.com>
2026-01-13 22:12:49 +0800

    diet

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'diet-tracker-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    // 如果是發送到 Google Script 的請求，不要快取，直接聯網
+    if (event.request.url.includes('script.google.com')) {
+        return; 
+    }
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
+});
